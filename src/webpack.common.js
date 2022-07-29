9035acd05dd95ef551df845bff454b0350c52f03
Enderlorde <ender.workpost@gmail.com>
2022-07-29 12:39:50 +0300

    switch to react/fiber, fixed bundling

---
+++ src/webpack.common.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const htmpWebackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/app.jsx',
+    output: {
+        clean: true,
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist/')
+    },
+    mode: 'production',
+    module:{
+        rules:[
+            {
+                test: /\.jsx$/i,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                options: {
+                    presets:['@babel/preset-react']
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                exclude: /(node_modules)/,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader"
+                  ]
+            },
+            {
+                test: /\.glb/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'models/[hash][ext][query]'
+                }
+            },
+            {
+                test: /\.png/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'textures/[hash][ext][query]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new htmpWebackPlugin({
+            template: './src/static/index.html',
+            filename: 'index.html',
+            title: 'webpack+react+threejs'
+        })
+    ]
+}
--- src/webpack.dev.js
+++ src/webpack.dev.js
@@ -1,23 +1,15 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
-const { resolve } = require('path');
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: 'eval-source-map',
+    devtool: 'inline-source-map',
     devServer: {
         static: {
             directory: path.join(__dirname, '../../dist/client')
         },
         historyApiFallback: true,
         hot: true
-    },
-    resolve:{
-        fallback:{
-            "util": false,
-            "path": false,
-            "fs": false
-        }
     }
 });
